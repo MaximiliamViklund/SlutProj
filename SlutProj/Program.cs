cfb7ced7d953f7331d24fd5fa208bc5632531a4a
Maximiliam Viklund <maximiliam.viklund@elev.ga.ntig.se>
2024-04-20 22:37:57 +0200

    Fixat buggar, ändrat initiative system, lagt in returnerande funktion samt lagt in kommentarer

--- SlutProj/Program.cs
+++ SlutProj/Program.cs
@@ -2,23 +2,41 @@
 using System.ComponentModel.Design;
 using System.Reflection;
 
+//-----------------------------Pseudokod---------------------------------//
+//A function for the main menu so that you can always return to it
+//A function for each part of the game
+//Class choosing menu
+//Store
+//Play
+//Put all of these as choices in the main menu functions
+//Use the readline function to read the users input in the menu
+//Use an if-statement to use different functions depending on input from readline
+//Regulate input by using else
 
-Character Active=new(null,-1,-1,-1,-1,-1);
-Character Opponent=new(null,-1,-1,-1,-1,-1);
-int activeBaseHp=0;
-int opponentBaseHp=0;
+//Edit: Maybe use switch instead of if?
+
+
+Character Active=new(null,-1,-1,-1,-1,-1); //Creates the Active Character that will be used to play
+Character Opponent=new(null,-1,-1,-1,-1,-1); //Creates the Opponent Character which will be played against
+int activeBaseHp=0; //Base hp used to return Acives health to the default hp of its class after fight
+int opponentBaseHp=0; //Base hp used to return Opponents health to the default hp of its class after fight
 bool play=false;
 
 Menu(Active,Opponent,play,activeBaseHp,opponentBaseHp);
 Console.ReadLine();
 
-static void Text(string text){
+static void Text(string text){ //Function takes string and writes it out in green
+    Console.ForegroundColor=ConsoleColor.Green;
     Console.WriteLine(text);
+    Console.ForegroundColor=ConsoleColor.White;
 }
+
+
+//-------------------------------MAIN MENU------------------------------------------------------------------//
 static void Menu(Character Active,Character Opponent, bool play, int activeBaseHp, int opponentBaseHp){
     string resp;
     Text("Welcome to THE FIGHTING GAME");
-    Console.WriteLine("Choose a class to play");
+    Text("Choose a class to play");
     Console.WriteLine("a) Choose Class");
     Console.WriteLine("b) Read Rules");
     Console.WriteLine("c) Store");
@@ -29,6 +47,8 @@ static void Menu(Character Active,Character Opponent, bool play, int activeBaseH
     }
     resp=Console.ReadLine().ToLower();
 
+
+    //Reads resp and uses different functions depending on input
     if(resp=="d"&&play==true){
         Console.Clear();
         Play(Active,Opponent,play,activeBaseHp,opponentBaseHp);
@@ -68,7 +88,7 @@ static void Menu(Character Active,Character Opponent, bool play, int activeBaseH
 
 static void Classes(Character Active, Character Opponent, bool play, int activeBaseHp, int opponentBaseHp){
     string resp;    //string to read ReadLines
-    int opponent;   //int to randomize Opponents Class
+    int resultat;
 
     //Creating classes and adding them to CharacterList
     //Choose to make a list because it enables me to easily add more charcter classes
@@ -84,28 +104,25 @@ static void Classes(Character Active, Character Opponent, bool play, int activeB
 
 
 
-    Random generator=new();
-
-    opponent=generator.Next(1,ClassList.Count);
-    Opponent=ClassList[opponent];
 
 
-    Console.WriteLine("Choose your Class:");
-    for (int i = 0; i < ClassList.Count; i++)
+    Text("Enter number associated with the class you want to play:");
+    for (int i = 0; i < ClassList.Count; i++)   //For loop writes out all available classes and the number the user needs to enter to choose each class
     {
         Console.WriteLine();
+        Console.ForegroundColor=ConsoleColor.Green;
         Console.WriteLine(i+1);
+        Console.ForegroundColor=ConsoleColor.White;
         ClassList[i].Stats();
     }
     resp=Console.ReadLine().ToLower();
-    bool failSafe = int.TryParse(resp, out int resultat);
+    bool failSafe = int.TryParse(resp, out resultat);
     resultat--;
 
 
-    if(failSafe==true&&resultat<=ClassList.Count){
+    if(failSafe==true&&resultat<ClassList.Count&&resultat>-1){ //makes sure that user cant input invalid numbers
         Active=ClassList[resultat];
         activeBaseHp=Active.hp;
-        opponentBaseHp=Opponent.hp;
     }
     else{
         Console.Clear();
@@ -114,16 +131,25 @@ static void Classes(Character Active, Character Opponent, bool play, int activeB
         Console.Clear();
         Classes(Active, Opponent,play,activeBaseHp,opponentBaseHp);
     }
+
+    int opponent=resultat;
+    Random generator=new();
+    while(opponent==resultat){opponent=generator.Next(0,ClassList.Count);} //Randomizes opponents class and makes sure it isn't the same as the users class
+    Opponent=ClassList[opponent];
+    opponentBaseHp=Opponent.hp;
+
     failSafe=false;
     Console.Clear();
     Console.WriteLine("Your character:");
     Active.Stats();
+    Console.WriteLine();
+    Opponent.Stats();
     Console.ReadLine();
     Console.Clear();
     Menu(Active,Opponent,play, activeBaseHp,opponentBaseHp);
 }
 
-static void Rules(Character Active, Character Opponent, bool play, int activeBaseHp, int opponentBaseHp){
+static void Rules(Character Active, Character Opponent, bool play, int activeBaseHp, int opponentBaseHp){ //Writes out rules for the game/how the game works
     Console.WriteLine("First, you choose a character");
     Console.WriteLine("Each character has different modifiers for damage, accuracy and initiative");
     Console.WriteLine("They also have different hp and armor");
@@ -134,7 +160,7 @@ static void Rules(Character Active, Character Opponent, bool play, int activeBas
     Console.WriteLine("You again roll a random number, this time between 1 and 12, and add your damage modifier");
     Console.WriteLine("First one to hit 0 hp loses");
     Console.WriteLine("Good luck!");
-    Console.WriteLine("Press ENTER");
+    Text("Press ENTER");
     Console.ReadLine();
     Console.Clear();
     Menu(Active,Opponent,play, activeBaseHp,opponentBaseHp);
@@ -143,10 +169,11 @@ static void Rules(Character Active, Character Opponent, bool play, int activeBas
 static void Store(){
 
 }
-
+//-----------------------------------PLAY---------------------------------------------//
 static void Play(Character Active, Character Opponent, bool play, int activeBaseHp, int opponentBaseHp){
     Random generator=new();
     int dice;
+    bool iniative=false;
 
     Console.WriteLine("Your Character:");
     Active.Stats();
@@ -156,32 +183,32 @@ static void Play(Character Active, Character Opponent, bool play, int activeBase
     Console.ReadLine();
     Console.Clear();
 
-    Console.WriteLine("Let's FIGHT!");
-    Console.WriteLine("Press ENTER");
+    Text("Let's FIGHT!");
+    Text("Press ENTER");
     Console.ReadLine();
     dice=generator.Next(1,21);
     int ActiveInitiative=dice+Active.speed;
-    Console.WriteLine("You rolled "+ActiveInitiative+" for initiative");
+    dice=generator.Next(1,21);
     int OpponentInitiative=dice+Opponent.speed;
+    if(ActiveInitiative>=OpponentInitiative){iniative=true;} //Makes boolean initiative true if users iniative is higher than the opponents
+    Console.WriteLine("You rolled "+ActiveInitiative+" for initiative");
     Console.WriteLine("Your opponent rolled "+OpponentInitiative+" for initiative");
+    if(iniative==true){Console.WriteLine("You will start");}else{Console.WriteLine("Your opponent will start");}
     Console.ReadLine();
     Console.Clear();
 
-    while(0<Active.hp&&0<Opponent.hp){
+
+    while(0<Active.hp&&0<Opponent.hp){ //Loop playing the game
         Console.WriteLine("Your health: "+Active.hp);
         Console.WriteLine("Opponents health: "+Opponent.hp);
         Console.WriteLine();
-        if(ActiveInitiative>=OpponentInitiative&&Active.hp!=0){
+        if(iniative==true&&Active.hp!=0){ //Makes sure that whoever has higher iniative plays first
             ActiveAttack(Active,Opponent,generator,dice);
-        }
-        else if(OpponentInitiative>ActiveInitiative&&Opponent.hp!=0){
             OpponentAttack(Active,Opponent,generator,dice);
         }
-        if(ActiveInitiative<OpponentInitiative&&Active.hp!=0){
-            ActiveAttack(Active,Opponent,generator,dice);
-        }
-        else if(OpponentInitiative<=ActiveInitiative&&Opponent.hp!=0){
+        else if(iniative==false&&Opponent.hp!=0){
             OpponentAttack(Active,Opponent,generator,dice);
+            ActiveAttack(Active,Opponent,generator,dice);
         }
 
         if(Opponent.hp==0){Console.WriteLine("Congrats, you won!");}
@@ -196,23 +223,23 @@ static void Play(Character Active, Character Opponent, bool play, int activeBase
     Menu(Active,Opponent,play,activeBaseHp,opponentBaseHp);
 
 
-    static void ActiveAttack(Character Active, Character Opponent, Random generator, int dice){
+    static void ActiveAttack(Character Active, Character Opponent, Random generator, int dice){ //Function for users attacks
         int adder=0;
+        int hitRoll=21;
+        int dmgRoll=13;
 
         Console.WriteLine();
-        Console.WriteLine("Press ENTER to attack");
+        Text("Press ENTER to attack");
         Console.ReadLine();
-        dice=generator.Next(1,21);
-        adder=dice+Active.accuracy;
+        adder=Modify(Active.accuracy,dice,adder,hitRoll); //Calculates hit roll
         Console.WriteLine("You rolled a "+adder);
         if(adder>=Opponent.armorClass){
             Console.WriteLine("You hit!");
-            Console.WriteLine("Press ENTER to roll for damage");
+            Text("Press ENTER to roll for damage");
             Console.ReadLine();
-            dice=generator.Next(1,13);
-            adder=dice+Active.dmg;
+            adder=Modify(Active.dmg,dice,adder,dmgRoll); //Calculates damage roll
             Console.WriteLine("You dealt "+adder+" damage");
-            Opponent.hp-=adder;
+            Opponent.hp-=adder; //subtracts damage roll from health
             if(Opponent.hp<0){Opponent.hp=0;}
             Console.WriteLine("Opponents health is now "+Opponent.hp);
             Console.ReadLine();
@@ -222,27 +249,34 @@ static void Play(Character Active, Character Opponent, bool play, int activeBase
             Console.ReadLine();
         }
     }
-    static void OpponentAttack(Character Active, Character Opponent, Random generator, int dice){
+    static void OpponentAttack(Character Active, Character Opponent, Random generator, int dice){ //Function for opponents attack
         int adder=0;
+        int hitRoll=21;
+        int dmgRoll=13;
 
         Console.WriteLine("Your Opponent attacks");
-        dice=generator.Next(1,21);
-        adder=dice+Opponent.accuracy;
+        adder=Modify(Opponent.accuracy,dice,adder,hitRoll); //Calculates hit roll
         Console.WriteLine("They rolled a "+adder);
 
         if(adder>=Active.armorClass){
             Console.WriteLine("Your Opponent hit");
             Console.ReadLine();
-            dice=generator.Next(1,13);
-            adder=dice+Opponent.dmg;
+            adder=Modify(Opponent.dmg,dice,adder,dmgRoll); //Calculates damage roll
             Console.WriteLine("They deal "+adder+" damage to you");
-            Active.hp-=adder;
+            Active.hp-=adder; //subtracts damage roll from health
             if(Active.hp<0){Active.hp=0;} 
             Console.WriteLine("Your health is now "+Active.hp);
             Console.ReadLine();
         }
         else{
             Console.WriteLine("And they missed");
+            Console.ReadLine();
         }
+    }
+    static int Modify(int Modifier, int dice, int adder, int roll){ //Function to calculate rolls
+        Random generator=new();
+        dice=generator.Next(1,roll);
+        adder=dice+Modifier;
+        return adder;
     }
 }
